21ca71ff1061ca02a7b15a57f1323aeca4656602
Acer <john.kosinski@gmail.com>
2024-03-16 22:02:16 +0700

    user operations with ERC20 paymaster

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config"
-import { getAccountNonce, createSmartAccountClient, ENTRYPOINT_ADDRESS_V07, bundlerActions, getSenderAddress, signUserOperationHashWithECDSA } from "permissionless"
-import { Address, createClient, createPublicClient, encodeFunctionData, http } from "viem"
+import { getAccountNonce, createSmartAccountClient, ENTRYPOINT_ADDRESS_V07, ENTRYPOINT_ADDRESS_V06, bundlerActions, getSenderAddress, signUserOperationHashWithECDSA } from "permissionless"
+import { Address, concat, createClient, createPublicClient, encodeFunctionData, Hash, http } from "viem"
 //import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
-import { sepolia } from "viem/chains"
+import { sepolia, polygonMumbai } from "viem/chains"
 import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
 import { privateKeyToSimpleSmartAccount, privateKeyToSafeSmartAccount } from "permissionless/accounts";
 import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico";
@@ -63,7 +63,7 @@ async function gaslessTransactionsTest() {
     console.log(smartAccountClient.account.address);
 }
 
-async function sendUserOperationsTest() {
+async function sendUserOperationsWithPaymasterTest() {
     const bundlerClient = createClient({
         transport: http(paymasterUrl),
         chain: sepolia,
@@ -186,9 +186,142 @@ async function sendUserOperationsTest() {
     const receipt = await bundlerClient.waitForUserOperationReceipt({
         hash: userOperationHash,
     })
-    const txHash = receipt.receipt.transactionHash
+    const txHash = receipt.receipt.transactionHash;
 
     console.log(`UserOperation included: https://sepolia.etherscan.io/tx/${txHash}`);
 }
 
-sendUserOperationsTest();
+async function sendUserOperationsWithErc20PaymasterTest() 
+{
+    const entryPointAddr = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"; 
+    const accountFactoryAddr = "0x9406Cc6185a346906296840746125a0E44976454"; 
+    
+    const signer = privateKeyToAccount(privateKey as Hash); 
+    
+    const bundlerClient = createClient({
+        //using v1 of the API
+        transport: http(`https://api.pimlico.io/v1/sepolia/rpc?apikey=${apiKey}`),
+        chain: sepolia,
+    }).extend(bundlerActions(ENTRYPOINT_ADDRESS_V06)
+    ).extend(pimlicoBundlerActions(ENTRYPOINT_ADDRESS_V06));
+    
+    const paymasterClient = createClient({
+        //using v2 of the API
+        transport: http(`https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`),
+        chain: sepolia,
+    }).extend(pimlicoPaymasterActions(ENTRYPOINT_ADDRESS_V06));
+
+    const publicClient = createPublicClient({
+        transport: http("https://sepolia.rpc.thirdweb.com"),
+        chain: sepolia,
+    });
+    
+    const initCode: `0x${string}` | Uint8Array | undefined = concat([
+        accountFactoryAddr,
+        encodeFunctionData({
+            abi: [
+                {
+                    inputs: [
+                        { name: "owner", type: "address" },
+                        { name: "salt", type: "uint256" },
+                    ],
+                    name: "createAccount",
+                    outputs: [{ name: "ret", type: "address" }],
+                    stateMutability: "nonpayable",
+                    type: "function",
+                },
+            ],
+            args: [signer.address, 0n],
+        }),
+    ]);
+
+    const senderAddress = await getSenderAddress(publicClient, {
+        initCode,
+        entryPoint: entryPointAddr,
+    })
+    console.log("Counterfactual sender address:", senderAddress); 
+    
+    //generateApproveCallData
+    const generateApproveCallData = (erc20TokenAddress: Address, paymasterAddress: Address) => {
+        const approveData = encodeFunctionData({
+            abi: [
+                {
+                    inputs: [
+                        { name: "_spender", type: "address" },
+                        { name: "_value", type: "uint256" },
+                    ],
+                    name: "approve",
+                    outputs: [{ name: "", type: "bool" }],
+                    payable: false,
+                    stateMutability: "nonpayable",
+                    type: "function",
+                },
+            ],
+            args: [paymasterAddress, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn],
+        })
+
+        // GENERATE THE CALLDATA TO APPROVE THE USDC
+        const to = erc20TokenAddress
+        const value = 0n
+        const data = approveData
+
+        const callData = encodeFunctionData({
+            abi: [
+                {
+                    inputs: [
+                        { name: "dest", type: "address" },
+                        { name: "value", type: "uint256" },
+                        { name: "func", type: "bytes" },
+                    ],
+                    name: "execute",
+                    outputs: [],
+                    stateMutability: "nonpayable",
+                    type: "function",
+                },
+            ],
+            args: [to, value, data],
+        })
+
+        return callData
+    }
+    
+    const submitUserOperation = async (userOperation: UserOperation<"v0.6">) => {
+        const userOperationHash = await bundlerClient.sendUserOperation({
+            userOperation,
+        })
+        console.log(`UserOperation submitted. Hash: ${userOperationHash}`)
+
+        //TODO: should be its own function (is repeated several times)
+        console.log("Querying for receipts...")
+        const receipt = await bundlerClient.waitForUserOperationReceipt({
+            hash: userOperationHash,
+        })
+        console.log(`Receipt found... transaction hash: ${receipt.receipt.transactionHash}`)
+    }
+    
+    // https://docs.pimlico.io/paymaster/erc20-paymaster/contract-addresses
+    const erc20PaymasterAddress = "0x0000000000325602a77416A16136FDafd04b299f"
+    const usdcTokenAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238" // USDC on sepolia
+
+    const approveCallData = generateApproveCallData(usdcTokenAddress, erc20PaymasterAddress);
+    console.log("approveCallData: " + approveCallData);
+    
+    const senderUsdcBalance = await publicClient.readContract({
+        abi: [
+            {
+                inputs: [{ name: "_owner", type: "address" }],
+                name: "balanceOf",
+                outputs: [{ name: "balance", type: "uint256" }],
+                type: "function",
+                stateMutability: "view",
+            },
+        ],
+        address: usdcTokenAddress,
+        functionName: "balanceOf",
+        args: [senderAddress],
+    }); 
+    
+    console.log("senderUsdcBalance:", senderUsdcBalance);
+}
+
+sendUserOperationsWithErc20PaymasterTest();
