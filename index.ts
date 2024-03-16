6a093e7515f8330bae2da1f74b9144d1bb9abe87
Acer <john.kosinski@gmail.com>
2024-03-16 21:17:17 +0700

    added api key

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 import "dotenv/config"
 import { getAccountNonce, createSmartAccountClient, ENTRYPOINT_ADDRESS_V07 } from "permissionless"
-import { UserOperation, bundlerActions, getSenderAddress, getUserOperationHash, waitForUserOperationReceipt, GetUserOperationReceiptReturnType, signUserOperationHashWithECDSA } from "permissionless"
-import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
-import { Address, Hash, concat, createClient, createPublicClient, encodeFunctionData, http, Hex } from "viem"
-import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
-import { lineaTestnet, polygonMumbai, sepolia } from "viem/chains"
+import { Address, createPublicClient, http } from "viem"
+//import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
+import { sepolia } from "viem/chains"
 import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
 import { privateKeyToSimpleSmartAccount, privateKeyToSafeSmartAccount } from "permissionless/accounts";
-import { writeFileSync } from 'fs'
 
-const apiKey = ""
-const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+const apiKey = "dfc7d1e4-804b-41dc-9be5-57084b57ea73";
+const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`;
 
 const privateKey = "0xb273a4b1cfde72170a1ff120127e9ff1d4304328b6ed055b85f140190968682b";
 
@@ -27,14 +24,14 @@ const account = await privateKeyToSimpleSmartAccount(publicClient, {
     privateKey,
     entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
     factoryAddress: "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
-})
+});
 
 const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
 
 const bundlerClient = createPimlicoBundlerClient({
     transport: http(bundlerUrl),
     entryPoint: ENTRYPOINT_ADDRESS_V07,
-})
+});
 
 const smartAccountClient = createSmartAccountClient({
     account,
@@ -47,15 +44,15 @@ const smartAccountClient = createSmartAccountClient({
         },
         sponsorUserOperation: paymasterClient.sponsorUserOperation,
     },
-})
+});
 
 const txHash = await smartAccountClient.sendTransaction({
     to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
     value: 0n,
     data: "0x1234",
-})
+});
 
-console.log(`User operation included: https://sepolia.etherscan.io/tx/${txHash}`)
+console.log(`User operation included: https://sepolia.etherscan.io/tx/${txHash}`);
 
 console.log(`Smart account address: https://sepolia.etherscan.io/address/${account.address}`);
 //console.log(paymasterClient);
